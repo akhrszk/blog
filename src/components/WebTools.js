0d1720fd002a5b2ea92362f725bf4ba2d77d0564
Akihiro Suzuki <28677705+akhrszk@users.noreply.github.com>
2023-03-25 04:11:54 +0900

    :sparkles: Add tools.akihiro.dev external links

--- src/components/WebTools.js
+++ src/components/WebTools.js
@@ -1,28 +1,34 @@
 import { Link } from "gatsby"
 import React from "react"
 
+const sites = [
+  {
+    title: "正規表現チェッカー",
+    url: "https://tools.akihiro.dev/regex-checker/",
+  },
+  { title: "JSON整形ツール", url: "https://tools.akihiro.dev/json-formatter/" },
+  {
+    title: "URL エンコーダー & デコーダー",
+    url: "https://tools.akihiro.dev/url-encoder-decoder/",
+  },
+]
+
 const WebTools = () => (
   <div>
     <h3 className="mb-3">Webツール</h3>
     <ul className="list-disc pl-6">
-      <li>
-        <Link
-          className="text-primary font-semibold"
-          to="/tools/time/"
-          itemProp="url"
-        >
-          時間
-        </Link>
-      </li>
-      <li>
-        <Link
-          className="text-primary font-semibold"
-          to="/tools/regex/"
-          itemProp="url"
-        >
-          正規表現チェッカー
-        </Link>
-      </li>
+      {sites.map(({ title, url }) => (
+        <li key={url}>
+          <Link
+            className="text-primary font-semibold"
+            to={url}
+            itemProp="url"
+            target="_blank"
+          >
+            {title}
+          </Link>
+        </li>
+      ))}
     </ul>
   </div>
 )
